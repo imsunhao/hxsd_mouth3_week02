eee564b62b904dff7d9b7f4da11aa1574995d6a0
imsunhao <sunhao__159@163.com>
2016-10-16 00:02:50 +0800

    homework was down but had a little bug

--- js/menu.js
+++ js/menu.js
@@ -1,81 +1,35 @@
-var Menu = (function() {
-	
-	var $container = $( '#rm-container' ),						
-		$cover = $container.find( 'div.rm-cover' ),
-		$middle = $container.find( 'div.rm-middle' ),
-		$right = $container.find( 'div.rm-right' ),
-		$open = $cover.find('a.rm-button-open'),
-		$close = $right.find('span.rm-close'),
-		$details = $container.find( 'a.rm-viewdetails' ),
+var Menu = (function () {
 
-		init = function() {
+    var $container = $('#rm-container'),
+        $cover = $container.find('div.rm-cover'),
+        $right = $container.find('div.rm-right'),
+        $open = $cover.find('a.rm-button-open'),
+        $close = $right.find('span.rm-close'),
 
-			initEvents();
+        init = function () {
+            initEvents();
+        },
+        initEvents = function () {
+            $open.on('click', function (event) {
+                openMenu('rm-open');
+                return false;
+            });
+            $close.on('click', function (event) {
 
-		},
-		initEvents = function() {
+                closeMenu('rm-open');
+                return false;
 
-			$open.on( 'click', function( event ) {
+            });
+        },
+        openMenu = function (Menu) {
 
-				openMenu();
-				return false;
+            $container.addClass(Menu);
 
-			} );
+        },
+        closeMenu = function (Menu) {
 
-			$close.on( 'click', function( event ) {
+            $container.removeClass(Menu + ' rm-nodelay rm-in');
 
-				closeMenu();
-				return false;
-
-			} );
-
-			$details.on( 'click', function( event ) {
-
-				$container.removeClass( 'rm-in' ).children( 'div.rm-modal' ).remove();
-				viewDetails( $( this ) );
-				return false;
-
-			} );
-			
-		},
-		openMenu = function() {
-
-			$container.addClass( 'rm-open' );
-
-		},
-		closeMenu = function() {
-
-			$container.removeClass( 'rm-open rm-nodelay rm-in' );
-
-		},
-		viewDetails = function( recipe ) {
-
-			var title = recipe.text(),
-				img = recipe.data( 'thumb' ),
-				description = recipe.parent().next().text(),
-				url = recipe.attr( 'href' );
-
-			var $modal = $( '<div class="rm-modal"><div class="rm-thumb" style="background-image: url(' + img + ')"></div><h5>' + title + '</h5><p>' + description + '</p><a href="' + url + '">See the recipe</a><span class="rm-close-modal">x</span></div>' );
-
-			$modal.appendTo( $container );
-
-			var h = $modal.outerHeight( true );
-			$modal.css( 'margin-top', -h / 2 );
-
-			setTimeout( function() {
-
-				$container.addClass( 'rm-in rm-nodelay' );
-
-				$modal.find( 'span.rm-close-modal' ).on( 'click', function() {
-
-					$container.removeClass( 'rm-in' );
-
-				} );
-			
-			}, 0 );
-
-		};
-
-	return { init : init };
-
+        };
+    return {init: init};
 })();
